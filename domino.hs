01632305aea5e25c5651760b4980c834381e0659
Ben <benas.kaz@gmail.com>
2017-11-14 21:38:28 +0000

    Final version, everything works, added comments, completed tests

--- domino.hs
+++ domino.hs
@@ -25,8 +25,8 @@ module Domino where
   createScores :: Hand -> Board -> End -> [Int]
   createScores [] _ _ = [] --Empty hand means empty score list(nothing can be scored)
   createScores hand@(h:t) board end
-    | playDom h board end == Nothing = [-1] ++  createScores t board end -- If domino CAN'T be played then put -1 to list and recursive call on tail
-    | otherwise = [scoreBoard(fromJust(playDom h board end))] ++ createScores t board end -- otherwise calculate score by putting it and recursive call on tail
+    | playDom h board end == Nothing = -1 : createScores t board end -- If domino CAN'T be played then put -1 to list and recursive call on tail
+    | otherwise = scoreBoard(fromJust(playDom h board end)) : createScores t board end -- otherwise calculate score by putting it and recursive call on tail
   
   -- Gets maximum score of 2 lists
   getMaxScore :: [Int] -> [Int] -> Int
@@ -42,7 +42,7 @@ module Domino where
   
   -- Plays a round of dominoes and returns the scores scored by both players
   playDomsRound :: DomsPlayer -> DomsPlayer -> Int -> (Int, Int)
-  playDomsRound p1 p2 seed = trace((show hand1) ++ " " ++ (show hand2))(playARound p1 p2 [] hand1 hand2, playARound p2 p1 (makeAMove p1 [] hand1) hand2 hand1) -- 2nd player starting board is board after 1st player's move
+  playDomsRound p1 p2 seed = (playARound p1 p2 [] hand1 hand2, playARound p2 p1 (makeAMove p1 [] hand1) hand2 hand1) -- 2nd player starting board is board after 1st player's move
     where hand1 = makeAHand 1 seed -- Creates hand for 1st player
           hand2 = makeAHand 2 seed -- Creates hand for 2nd player
         
@@ -52,7 +52,7 @@ module Domino where
   playARound p1 p2 board h1 h2
     | knockingP h1 board && knockingP h2 board = 0 -- Both players are knocking then score is 0
     | knockingP h1 board = 0 + playARound p1 p2 (makeAMove p2 (makeAMove p1 board h1) h2) h1 h2 -- If player 1 is knocking then move's score is 0 and recursive call
-    | otherwise = trace ("Putting " ++ show ( (p1 h1 board))++" Board: " ++ show (makeAMove p1 board h1)) snd (makeAMoveAndCalcScore p1 board h1) + playARound p1 p2 (makeAMove p2 (makeAMove p1 board h1) h2) h1 h2 -- add score and recursive call
+    | otherwise = snd (makeAMoveAndCalcScore p1 board h1) + playARound p1 p2 (makeAMove p2 (makeAMove p1 board h1) h2) h1 h2 -- add score and recursive call
     
   --Makes a move and returns a board
   makeAMove :: DomsPlayer -> Board -> Hand -> Board
@@ -78,10 +78,10 @@ module Domino where
   simplePlayer_Test2 = simplePlayer [(0,5), (2,4), (3,4), (6,2)] [] -- Result: Just ((0,5),L). Testing empty board.
   simplePlayer_Test3 = simplePlayer [(0,5), (0,6)] [(0,0)] -- Result: Just ((0,5),L). Testing if first domino will be placed(both can be).
   
-  hsdPlayer_Test0 = hsdPlayer [(0,3), (2,4), (3,4), (6,2)] [(3,5), (5,4)] -- Result: ((0,3),L).Testing RIGHT end(Scores 2 points) 
-  hsdPlayer_Test1 = hsdPlayer [(0,3), (3,1)] [(3,6), (6,4)] -- Result: Just ((3,1),L). Testing LEFT end.
-  hsdPlayer_Test2 = hsdPlayer [(0,5), (2,4), (6,6), (6,2)] [] -- Result: Just ((6,6),R). Testing empty board.
-  hsdPlayer_Test3 = hsdPlayer [(0,5), (0,6)] [(0,0)] -- Result: Just ((0,6),R). Testing if most scored domino will be placed(both can be).
+  hsdPlayer_Test0 = hsdPlayer [(0,3), (2,4), (3,4), (6,2)] [(3,5), (5,4)] -- Result: ((3,4),R). Testing right end(Scores 2 points) 
+  hsdPlayer_Test1 = hsdPlayer [(4,1), (3,1)] [(3,6), (6,4)] -- Result: ((3,1),L). Testing left end.
+  hsdPlayer_Test2 = hsdPlayer [(0,5), (2,4), (6,6), (6,2)] [] -- Result: ((6,6),R). Testing empty board.
+  hsdPlayer_Test3 = hsdPlayer [(0,5), (0,6)] [(0,0)] -- Result: ((0,6),R).
   
   shuffleDoms_Test0 = shuffleDoms 0 -- Result: [(0,5),(2,3),(0,1),(3,6),(1,3),(3,4),(1,5),(2,5),(4,4),(3,5),(3,3),(5,5),(2,6),(0,3),(0,6),(1,4),(4,5),(6,6),(0,2),(1,1),(1,2),(5,6),(4,6),(0,4),(2,2),(2,4),(1,6),(0,0)]
   shuffleDoms_Test1 = shuffleDoms 1 -- Result: [(1,3),(1,1),(0,4),(4,5),(1,6),(6,6),(5,5),(0,6),(4,6),(0,5),(3,4),(4,4),(1,5),(0,1),(3,3),(0,2),(2,3),(1,4),(3,5),(2,4),(2,5),(1,2),(2,2),(0,0),(3,6),(2,6),(0,3),(5,6)]
@@ -117,37 +117,38 @@ module Domino where
     p1: Putting ((3,4),R) Board: [(3,6),(6,1),(1,3),(3,2),(2,4),(4,3)].   Scores: (8, 0)
   --}
   
-  playDomsRound_Test2 = playDomsRound simplePlayer hsdPlayer 2 -- Result: (13,9)
+  playDomsRound_Test2 = playDomsRound simplePlayer hsdPlayer 2 -- Result: (11,9)
   {--
   How the game is played:
     Hand1: [(3,3),(0,3),(1,4),(2,6),(3,4),(0,1),(0,4)] Hand2: [(3,6),(5,6),(5,5),(2,5),(1,1),(6,6),(0,5)]
-    p1: Putting ((3,3),L) Board: [(3,3)]. Scores (4,0)
-    p2: Putting ((3,6),R) Board: [(3,3),(3,6)]. Scores (4,4)
-    p1: Putting ((0,3),L) Board: [(0,3),(3,3),(3,6)]. Scores (6,4)
-    p2: Putting ((6,6),R) Board: [(0,3),(3,3),(3,6),(6,6)] Score 4. Scores (6,8)
-    p1: Putting ((2,6),R) Board: [(0,3),(3,3),(3,6),(6,6),(6,2)] Score 0. Scores (6,8)
-    p2: Putting ((2,5),R) Board: [(0,3),(3,3),(3,6),(6,6),(6,2),(2,5)] Score 1. Scores (6,9)
-    p1: Putting ((0,1),L) Board: [(1,0),(0,3),(3,3),(3,6),(6,6),(6,2),(2,5)] Score 2. Scores (8,9)
-    p2: Putting ((5,6),R) Board: [(1,0),(0,3),(3,3),(3,6),(6,6),(6,2),(2,5),(5,6)] Score 0. Scores (8,9)
-    p1: Putting ((1,4),L) Board: [(4,1),(1,0),(0,3),(3,3),(3,6),(6,6),(6,2),(2,5),(5,6)] Score 2. Scores (10,9)
-    p2: CAN'T PUT ANYTHING(SKIPS). Scores (10,9)
-    p1: Putting ((3,4),L) Board: [(3,4),(4,1),(1,0),(0,3),(3,3),(3,6),(6,6),(6,2),(2,5),(5,6)] Score 3. Scores (13,9)
+    p1: Putting ((3,3),L) Board: [(3,3)]. Scores (2,0)
+    p2: Putting ((3,6),R) Board: [(3,3),(3,6)]. Scores (2,4)
+    p1: Putting ((0,3),L) Board: [(0,3),(3,3),(3,6)]. Scores (4,4)
+    p2: Putting ((6,6),R) Board: [(0,3),(3,3),(3,6),(6,6)]. Scores (4,8)
+    p1: Putting ((2,6),R) Board: [(0,3),(3,3),(3,6),(6,6),(6,2)]. Scores (4,8)
+    p2: Putting ((2,5),R) Board: [(0,3),(3,3),(3,6),(6,6),(6,2),(2,5)]. Scores (4,9)
+    p1: Putting ((0,1),L) Board: [(1,0),(0,3),(3,3),(3,6),(6,6),(6,2),(2,5)] . Scores (6,9)
+    p2: Putting ((5,6),R) Board: [(1,0),(0,3),(3,3),(3,6),(6,6),(6,2),(2,5),(5,6)] . Scores (6,9)
+    p1: Putting ((1,4),L) Board: [(4,1),(1,0),(0,3),(3,3),(3,6),(6,6),(6,2),(2,5),(5,6)] . Scores (8,9)
+    p2: CAN'T PUT ANYTHING(SKIPS). Scores (8,9)
+    p1: Putting ((3,4),L) Board: [(3,4),(4,1),(1,0),(0,3),(3,3),(3,6),(6,6),(6,2),(2,5),(5,6)] . Scores (11,9)
   --}
   
   playDomsRound_Test3 = playDomsRound hsdPlayer simplePlayer 5 -- Result: (10,6)
   {--
   How the game is played:
-  Hand1: [(2,6),(2,3),(2,4),(0,6),(5,6),(5,5),(1,1)] Hand2: [(2,5),(3,6),(1,2),(4,4),(0,0),(3,3),(4,6)]
-  p1: Putting ((5,5),R) Board: [(5,5)]. Scores (2,0)
-  p2: Putting ((2,5),L) Board: [(2,5),(5,5)]. Scores (2,0)
-  p1: Putting ((2,6),L) Board: [(6,2),(2,5),(5,5)].  Scores (2,0)
-  p1: Putting ((2,3),L) Board: [(2,3),(3,6),(6,2),(2,5),(5,5)].  Scores (,)
-  p1: Putting ((1,1),L) Board: [(1,1),(1,2),(2,3),(3,6),(6,2),(2,5),(5,5)].  Scores (,)
-  p1: Putting ((5,6),R) Board: [(1,1),(1,2),(2,3),(3,6),(6,2),(2,5),(5,5),(5,6)].  Scores (,)
-  p1: Putting ((2,4),R) Board: [(1,1),(1,2),(2,3),(3,6),(6,2),(2,5),(5,5),(5,6),(6,4),(4,2)].  Scores (,)
-  p2: Putting ((3,6),L) Board: [(3,6),(6,2),(2,5),(5,5)].  Scores (,)
-  p2: Putting ((1,2),L) Board: [(1,2),(2,3),(3,6),(6,2),(2,5),(5,5)].  Scores (,)
-  p2: Putting ((4,6),R) Board: [(1,1),(1,2),(2,3),(3,6),(6,2),(2,5),(5,5),(5,6),(6,4)].  Scores (,)
+    Hand1: [(2,6),(2,3),(2,4),(0,6),(5,6),(5,5),(1,1)] Hand2: [(2,5),(3,6),(1,2),(4,4),(0,0),(3,3),(4,6)]
+    p1: Putting ((5,5),R) Board: [(5,5)]. Scores (2,0)
+    p2: Putting ((2,5),L) Board: [(2,5),(5,5)]. Scores (2,4)
+    p1: Putting ((2,6),L) Board: [(6,2),(2,5),(5,5)].  Scores (2,4)
+    p2: Putting ((3,6),L) Board: [(3,6),(6,2),(2,5),(5,5)].  Scores (2,4)
+    p1: Putting ((2,3),L) Board: [(2,3),(3,6),(6,2),(2,5),(5,5)].  Scores (6,4)
+    p2: Putting ((1,2),L) Board: [(1,2),(2,3),(3,6),(6,2),(2,5),(5,5)].  Scores (6,4)
+    p1: Putting ((1,1),L) Board: [(1,1),(1,2),(2,3),(3,6),(6,2),(2,5),(5,5)].  Scores (10,4).
+    p2: CAN'T PUT ANYTHING(SKIPS). Scores (10,4)
+    p1: Putting ((5,6),R) Board: [(1,1),(1,2),(2,3),(3,6),(6,2),(2,5),(5,5),(5,6)].  Scores (10,4)
+    p2: Putting ((4,6),R) Board: [(1,1),(1,2),(2,3),(3,6),(6,2),(2,5),(5,5),(5,6),(6,4)].  Scores (10,6)
+    p1: Putting ((2,4),R) Board: [(1,1),(1,2),(2,3),(3,6),(6,2),(2,5),(5,5),(5,6),(6,4),(4,2)].  Scores (10,6)
   --}
   
   
@@ -253,6 +254,3 @@ module Domino where
     |playDom (head dominos) board end == Nothing = [] ++ scoreNEnd board (tail dominos) int end -- If domino can't go to any end then returning empty list plus recursive call on the tail of dominos
     |scoreBoard (fromJust (playDom (head dominos) board end)) == int = [head dominos] ++ scoreNEnd board (tail dominos) int end -- If score required can be get by playing domino at given end then adding that domino to recursive call on the tail of dominos
     |otherwise = [] ++ scoreNEnd board (tail dominos) int end -- Otherwise returning empty set plus recursivve call on tail of dominos
-  
-
- 
